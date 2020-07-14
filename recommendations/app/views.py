c8d119478cec71169c3e451e9c11213f9297d75e
Dmitry Karpov <dem0schase@gmail.com>
2020-07-15 00:31:50 +0300

    v2

--- recommendations/app/views.py
+++ recommendations/app/views.py
@@ -7,6 +7,14 @@ from .serializers import CategorySerializer, ProductSerializer, PropertySerializ
     CategoryKeyPropertySerializer
 
 
+def check_product_props(current_product, key_props):
+    prod_props = current_product.properties.filter(property__in=key_props)
+    return {
+        'prod_props': prod_props,
+        'mismatch': len(prod_props) < len(key_props),  # check if product misses some of its category key props
+    }
+
+
 class CategoryViewSet(ModelViewSet):
     serializer_class = CategorySerializer
     queryset = Category.objects.all()
@@ -33,6 +41,7 @@ class CategoryViewSet(ModelViewSet):
         result = []
         for property_row in properties:
             row = {}
+            row['row_id'] = property_row.id
             row['property_id'] = property_row.property.id
             row['property_name'] = property_row.property.name
             result.append(row)
@@ -41,14 +50,88 @@ class CategoryViewSet(ModelViewSet):
 
     @action(detail=False)
     def recommendations(self, request):
+
+        max_price_delta = request.query_params.get('price_delta')
+        try:
+            max_price_delta = float(max_price_delta)
+        except (TypeError, ValueError):
+            max_price_delta = 30.00
+
         result = []
         for category in Category.objects.all():
-            row = {}
-            row['id'] = category.id
-            row['name'] = category.name
-            result.append(row)
-        data = {'recommendations': result}
-        return Response(data=data)
+
+            cat_key_props = category.key_properties.all()
+            cat_props = [cat_key_prop.property for cat_key_prop in cat_key_props]
+            cat_prods_props = {}
+
+            for product in category.products.all():
+
+                key_props = []
+                similar_products = []
+                cat_prods_props[product.id] = {
+                    'product_id': product.id,
+                    'product_name': product.name,
+                    'product_price': product.price,
+                    'key_props': key_props,
+                    'similar_products': similar_products,
+                }
+
+                prod_data = check_product_props(product, cat_props)
+                prod_props = prod_data['prod_props']
+                mismatch = prod_data['mismatch']
+                if mismatch:
+                    continue
+
+                # product key props values
+                for property_row in prod_props.all():
+                    row = {}
+                    row['property_id'] = property_row.property.id
+                    row['property_name'] = property_row.property.name
+                    row['property_value'] = property_row.value
+                    key_props.append(row)
+                cat_prods_props[product.id]['key_props'] = key_props
+
+                # similar products
+                prod_props_values = {}
+                for prod_prop_row in prod_props:
+                    prod_props_values[prod_prop_row.property.id] = prod_prop_row.value
+
+                for curr_product in category.products.exclude(id=product.id):
+                    price_delta = (product.price - curr_product.price) / curr_product.price * 100
+                    price_delta = -price_delta if price_delta < 0 else price_delta
+                    price_delta = round(price_delta, 2)
+
+                    # price delta exceeds max allowed value
+                    if price_delta > max_price_delta:
+                        continue
+
+                    curr_prod_data = check_product_props(curr_product, cat_props)
+                    curr_prod_props = curr_prod_data['prod_props']
+                    curr_mismatch = curr_prod_data['mismatch']
+                    if curr_mismatch:
+                        continue
+
+                    for curr_property_row in curr_prod_props:
+                        # curr prod prop value did not match pivot prod prop value
+                        if curr_property_row.value != prod_props_values[curr_property_row.property.id]:
+                            break
+                    else:
+                        similar_products.append({
+                            'product_id': curr_product.id,
+                            'product_name': curr_product.name,
+                            'product_price': curr_product.price,
+                            'product_price_delta': price_delta,
+                        })
+
+                cat_prods_props[product.id]['similar_products'] = similar_products
+
+            result.append({
+                'category_id': category.id,
+                'category_name': category.name,
+                'products': cat_prods_props,
+            })
+
+        return Response(data=result)
 
 
 class ProductViewSet(ModelViewSet):
@@ -62,6 +145,7 @@ class ProductViewSet(ModelViewSet):
         result = []
         for property_row in properties:
             row = {}
+            row['row_id'] = property_row.id
             row['property_id'] = property_row.property.id
             row['property_name'] = property_row.property.name
             row['property_value'] = property_row.value
